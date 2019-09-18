5817739becbdb6a0709c08b501ddc0183b3495f2
przemyslawj <jarzeb5@gmail.com>
2019-09-18 11:32:40 +0100

    Small fixes in env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -2,7 +2,7 @@
 mkdir src && cd src && git clone --depth 1 https://github.com/flatironinstitute/CaImAn.git
 conda install -c anaconda cython
 conda create -n caiman -c conda-forge caiman
-source activate caiman && pip install moviepy tifffile ipyparallel peakutils moviepy
-#cd src/CaImAn && source activate caiman && pip install .
-source activate caiman && caimanmanager.py install
+conda activate caiman && pip install moviepy tifffile ipyparallel peakutils moviepy
+#cd src/CaImAn && conda activate caiman && pip install -e .
+conda activate caiman && caimanmanager.py install
 
