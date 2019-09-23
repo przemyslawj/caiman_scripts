6a45bee793a290af1c188f850c8e2890728ac624
przemyslawj <jarzeb5@gmail.com>
2019-09-23 16:39:49 +0100

    Modified the pipeline to run on multiple data dirs

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,94 @@
 #!/bin/bash
+# ./pipeline.sh animals_array dates_array
+# example: ./pipeline --animals "E-BL E-TR" --dates "2019-08-27" --dry_run
+
+EXP_MONTH=2019-08
+EXP_TITLE=habituation
+dry_run=0
+# load defaults
+source vars_setup.sh
+
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --dry_run)
+        dry_run=1
+        shift # past argument
+        ;;
+        --animals)
+        animals="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --dates)
+        dates="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --exp_title)
+        EXP_TITLE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --exp_month)
+        EXP_MONTH="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    esac
+done
+
+echo "Starting pipeline for exp_month=${EXP_MONTH} exp_title=${EXP_TITLE}"
+echo "                      animals=${animals} dates=${dates}"
 
 conda activate caiman
-source vars_setup.sh
-./gdrive_download.sh
-python downsample.py
-#./gdrive_download_processed.sh
-time python caiman_mc.py
-python memmap_mc_files.py
-python create_sessions_info.py
-python run_cnmfe.py
-./gdrive_upload.sh
+for exp_date in ${dates[*]}; do
+    for animal in ${animals[*]}; do
+        export ANIMAL=$animal
+        export EXP_DATE=$exp_date
+        export EXP_MONTH
+        export EXP_TITLE
+        echo "Running pipeline for animal=${ANIMAL} date=${EXP_DATE}"
+
+        if [ $dry_run -eq 1 ]; then
+            continue
+        fi
+
+        ./gdrive_download.sh
+
+        python downsample.py
+        status=$?
+        if [ $status -ne 0 ]; then
+            exit $status
+        fi
+
+        #./gdrive_download_processed.sh
+        time python caiman_mc.py
+        status=$?
+        if [ $status -ne 0 ]; then
+            exit $status
+        fi
+
+        python memmap_mc_files.py
+        status=$?
+        if [ $status -ne 0 ]; then
+            exit $status
+        fi
+
+        python create_sessions_info.py
+        status=$?
+        if [ $status -ne 0 ]; then
+            exit $status
+        fi
+
+        time python run_cnmfe.py
+        status=$?
+        if [ $status -ne 0 ]; then
+            exit $status
+        fi
+
+        ./gdrive_upload.sh
+    done
+done
 
--- vars_setup.sh
+++ vars_setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-export EXP_MONTH=2019-08
-export EXP_TITLE=habituation
-export EXP_DATE=2019-08-27
-export ANIMAL=E-BL
+export NCORES=4
 export DOWNSAMPLE=2
+
 export TRIAL_REL_DIR="${EXP_MONTH}/${EXP_TITLE}/${EXP_DATE}/trial/mv_caimg/${ANIMAL}/"
 export HOME_REL_DIR="${EXP_MONTH}/${EXP_TITLE}/${EXP_DATE}/homecage/mv_caimg/${ANIMAL}/"
 export TEST_REL_DIR="${EXP_MONTH}/${EXP_TITLE}/${EXP_DATE}/test/mv_caimg/${ANIMAL}/"
 export CAIMAN_REL_DIR="${EXP_MONTH}/${EXP_TITLE}/${EXP_DATE}/caiman/${ANIMAL}/"
-export NCORES=4
 
 #export RCLONE_CONFIG=drive_synced
 export RCLONE_CONFIG=workdrive
